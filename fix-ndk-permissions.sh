9a208b41274cbc485a5e2cf99976c7da59425aa6
Leo Davis <ldav1s@yahoo.com>
2014-05-23 13:59:49 -0600

    Added comments.

--- fix-ndk-permissions.sh
+++ fix-ndk-permissions.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+# fix-ndk-permissions.sh -- Fix permissions in an Android NDK/SDK drop so that
+# the compiler, headers, documentation, etc. so that the
+# owner:group may be root:root and still work.
+#
+# Just like the distro's native compiler!
+#
 find . -type f -not -perm -o=r -print0 | xargs -0 chmod o+r
 find . -type d -not -perm -o=r,o=x -print0 | xargs -0 chmod o+rx
 find . -type f \( -perm -u=x -and  -not -perm -o=x \) -print0 | xargs -0 chmod o+x
